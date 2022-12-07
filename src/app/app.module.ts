22a5b6b7666c11edbbaecacf3c194eccdf607910
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2022-12-07 20:50:51 +0000

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MyRouterlinkDirective } from './my-router-link.directive';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, MyRouterlinkDirective],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
